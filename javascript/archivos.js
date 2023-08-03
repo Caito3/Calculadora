e8b78369ffd6070f6ecb5ec0e3d27e93b7de3266
Carlos Julian Lopez <milkanojanich@gmail.com>
2023-08-03 11:55:05 -0300

    cambiar

--- javascript/archivos.js
+++ javascript/archivos.js
@@ -27,7 +27,7 @@ function botones(x){
     }
     if(aaa.length >= 3){
         totalTexto.style.fontSize="230%"
-        totalTexto.style.marginTop="10.3%"
+        totalTexto.style.marginTop="13%"
         aaa += texto
         totalTexto.innerHTML= aaa
     }else{
